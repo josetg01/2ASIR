e7a9c5d168fc1f6b804ca45654fcdb8d5ba704b2
josetg01 <35702013+josetg01@users.noreply.github.com>
2024-11-11 19:23:55 +0100

    Update install.sh

--- Administracion Sistemas Operativos/PROYECTO/parte2/install.sh
+++ Administracion Sistemas Operativos/PROYECTO/parte2/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-#
-wget 
+SCRIPT_MONITOR="/etc/monitoreo.sh"
+#SCRIPT DE MONITOREO
+sudo wget https://raw.githubusercontent.com/josetg01/2ASIR/refs/heads/main/Administracion%20Sistemas%20Operativos/PROYECTO/parte2/monitoreo.sh -O $SCRIPT_MONITOR
