ffe92a8824c8c95a05e27c016d61299318c59480
josetg01 <35702013+josetg01@users.noreply.github.com>
2024-11-11 19:26:15 +0100

    Update install.sh

--- Administracion Sistemas Operativos/PROYECTO/parte2/install.sh
+++ Administracion Sistemas Operativos/PROYECTO/parte2/install.sh
@@ -2,3 +2,21 @@
 SCRIPT_MONITOR="/etc/monitoreo.sh"
 #SCRIPT DE MONITOREO
 sudo wget https://raw.githubusercontent.com/josetg01/2ASIR/refs/heads/main/Administracion%20Sistemas%20Operativos/PROYECTO/parte2/monitoreo.sh -O $SCRIPT_MONITOR
+
+#Establecimiento del servicio de monitoreo
+cat > /etc/systemd/system/monitorizacion.service <<EOL
+[Unit]
+Description=Servicio de supervisión del sistema
+After=network.target
+
+[Service]
+ExecStart=$SCRIPT_MONITOR
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo systemctl enable monitorizacion
+sudo systemctl start monitorizacion
